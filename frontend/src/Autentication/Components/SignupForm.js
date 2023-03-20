65416fcbb08b76e02f03107ff052d29c5e31f57e
Yash Kathe <yashkathe123@gmail.com>
2023-03-20 22:22:08 +0530

    image upload component added

--- frontend/src/Autentication/Components/SignupForm.js
+++ frontend/src/Autentication/Components/SignupForm.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
+
 import FormLabel from "../../Shared/Components/FormLabel";
+import ImageUpload from "../../Shared/Components/ImageUpload"; 
 
 import styles from './Auth.module.css';
 
@@ -8,6 +10,7 @@ const SignupForm = (props) => {
     const [ emailID, setEmailID ] = useState('');
     const [ password, setPassword ] = useState('');
     const [ confirmPassword, setConfirmPassword ] = useState('');
+    const [ profileImage, setProfileImage ] = useState(null)
 
     const emailChangeHandler = (event) => {
         setEmailID(event.target.value);
@@ -21,9 +24,14 @@ const SignupForm = (props) => {
         setConfirmPassword(event.target.value);
     };
 
+    const profileImageHandler = (id='123', pickedFile, fileIsValid) => {
+        setProfileImage(pickedFile)
+    }
 
     const submitHandler = async (event) => {
         event.preventDefault();
+
+        console.log(profileImage)
 
         const user = {
             email: emailID,
@@ -34,6 +42,7 @@ const SignupForm = (props) => {
         setEmailID('');
         setPassword('');
         setConfirmPassword('');
+        setProfileImage(null)
     };
 
     return (
@@ -41,6 +50,7 @@ const SignupForm = (props) => {
             <FormLabel name="email" label="Email Address" type="email" changeHandler={ emailChangeHandler } value={ emailID } required={ true } />
             <FormLabel name="password" label="Password" type="password" changeHandler={ passwordChangeHandler } value={ password } required={ true } min="8" max="12" />
             <FormLabel name="confirmPassword" label="Confirm Password" type="password" changeHandler={ confirmPasswordChangeHandler } value={ confirmPassword } required={ true } min="8" max="12" />
+            <ImageUpload id="image" onInput={profileImageHandler}/>
             <div className={ styles.form_element }>
                 <button type="submit" >SIGN UP</button>
             </div>
---
+++ frontend/src/Shared/Components/ImageUpload.js
@@ -0,0 +1,73 @@
+import React, { useRef, useState, useEffect } from 'react';
+
+import styles from './ImageUpload.module.css';
+
+const ImageUpload = (props) => {
+
+    const [ file, setFile ] = useState();
+    const [ previewUrl, setPreviewUrl ] = useState();
+    const [ isValid, setIsValid ] = useState(false);
+
+    const filePickerRef = useRef();
+
+    useEffect(() => {
+        if(!file) {
+            return;
+        }
+        const fileReader = new FileReader();
+        fileReader.onload = () => {
+            setPreviewUrl(fileReader.result);
+        };
+        fileReader.readAsDataURL(file);
+    }, [ file ]);
+
+    const pickedImageHandler = (event) => {
+        //triggered when file is uploaded.
+        // so manage preview image state and pass it to the form so it can be uploaded to the backend
+        let pickedFile;
+        let fileIsValid = isValid;
+        if(event.target.files || event.target.files.length === 1) {
+            pickedFile = event.target.files[ 0 ];
+            setFile(pickedFile);
+            setIsValid(true);
+            fileIsValid = true;
+        } else {
+            setIsValid(false);
+            fileIsValid = false;
+        }
+        props.onInput(props.id, pickedFile, fileIsValid);
+    };
+
+    const pickImageHandler = () => {
+        //utilise the input element without seeing it 
+        filePickerRef.current.click();
+    };
+
+    return (
+        <div className={ styles.parentDiv }>
+            <input
+                id={ props.id }
+                type="file"
+                ref={ filePickerRef }
+                onChange={ pickedImageHandler }
+                style={ { display: 'none' } }
+                accept=".jpg, .jpeg, .png"
+            />
+            <div className={ styles[ 'parentDiv' ] }>
+                {
+                    previewUrl && (
+                        <div className={ styles[ 'image-upload__preview' ] }>
+                            <img src={ previewUrl } alt="preview" />
+                        </div>
+                    )
+                }
+                <button type="button" onClick={ pickImageHandler }>
+                    Pick Image
+                </button>
+            </div>
+            { !isValid && <p>{ props.errorText }</p> }
+        </div>
+    );
+};
+
+export default ImageUpload;
