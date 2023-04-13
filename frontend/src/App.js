5de2ec3a1484bc2c71e5f1682640bb00a96a55bc
Yash Kathe <yashkathe123@gmail.com>
2023-04-13 18:33:49 +0530

    added auto-login and auto-logout feature

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
 import Coupons from "./Coupons/Pages/Coupons";
@@ -13,20 +13,52 @@ import { AuthContext } from "./Shared/Context/auth-context";
 
 import styles from './App.module.css';
 
+let logoutTimer
+
 const App = () => {
 
     const [ token, setToken ] = useState(null);
+    const [ tokenExpirationTime, setTokenExpirationTime ] = useState();
     const [ userId, setUserId ] = useState(false);
 
-    const login = useCallback((uid, token) => {
+    const login = useCallback((uid, token, expirationDate) => {
         setToken(token);
+        const tokenExpirationDate = expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
+        setTokenExpirationTime(tokenExpirationDate);
+        localStorage.setItem('userData', JSON.stringify({
+            userId: uid,
+            token: token,
+            expiration: tokenExpirationDate.toISOString()
+        }));
         setUserId(uid);
     }, []);
 
     const logout = useCallback(() => {
         setToken(null);
+        setTokenExpirationTime(null)
         setUserId(null);
+        localStorage.removeItem('userData');
     }, []);
+
+    useEffect(() => {
+        if(token && tokenExpirationTime) {
+            const remainingTime = tokenExpirationTime.getTime() - new Date().getTime();
+            logoutTimer = setTimeout(logout, remainingTime);
+        } else {
+            clearTimeout(logoutTimer);
+        }
+    }, [ token, logout, tokenExpirationTime ]);
+
+    useEffect(() => {
+        const storedData = JSON.parse(localStorage.getItem('userData'));
+        if(
+            storedData &&
+            storedData.token &&
+            new Date(storedData.expiration) > new Date()
+        ) {
+            login(storedData.userId, storedData.token);
+        }
+    }, [ login ]);
 
     let routes;
 
@@ -69,7 +101,7 @@ const App = () => {
         <AuthContext.Provider
             value={ {
                 isLoggedIn: !!token,
-                token:token,
+                token: token,
                 userId,
                 login,
                 logout
